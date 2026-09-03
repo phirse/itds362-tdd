15d1c9f19f9a9b6dbef49c047e89f50bd75a5507
Pisute <portncf112st@gmail.com>
2026-09-03 11:52:41 +0700

    A1

---
+++ kitchen.py
@@ -0,0 +1,13 @@
+class Quantity:
+    def __init__(self, amount):
+        self.amount = amount
+ 
+    def times(self, multiplier):
+        return Quantity(self.amount * multiplier)
+    
+    def __eq__(self, other):
+        return self.amount == other.amount
+ 
+    def __repr__(self):
+        return f"Quantity({self.amount})"
+
