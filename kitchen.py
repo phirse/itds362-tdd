e2f0fd50d9151fe767fe72e5d3af21bd196bd70d
Pisute <portncf112st@gmail.com>
2026-09-03 11:58:43 +0700

    A4 mod;kitchen.py & test_kitchen.py

--- kitchen.py
+++ kitchen.py
@@ -4,3 +4,8 @@ class Quantity:
  
     def times(self, multiplier):
         return Quantity(self.amount * multiplier)
+    def __eq__(self, other):
+        return self.amount == other.amount
+ 
+    def __repr__(self):
+        return f"Quantity({self.amount})"
