3c8c1d04da969cbf520572b3f14b05dc3a2de0b7
Pisute <portncf112st@gmail.com>
2026-09-03 12:05:04 +0700

    A5

--- test_kitchen.py
+++ test_kitchen.py
@@ -7,21 +7,29 @@
 # ☐  (200 g + 1 oz) × 2
 
 from kitchen import Quantity
- 
- 
+
 def test_multiplication():
-    flour = Quantity(200)
-    assert flour.times(3) == Quantity(600)
+    flour = grams(200)
+    assert flour.times(3) == grams(600)
 
 def test_multiplication_by_two():
-    flour = Quantity(200)
-    assert flour.times(2) == Quantity(400) 
+    flour = grams(200)
+    assert flour.times(2) == grams(400) 
 
 def test_multiplication_returns_a_new_quantity():
-    flour = Quantity(200)
-    assert flour.times(3) == Quantity(600) 
-    assert flour.times(2) == Quantity(400) 
+    flour = grams(200)
+    assert flour.times(3) == grams(600) 
+    assert flour.times(2) == grams(400) 
 
 def test_equality():
-    assert Quantity(200) == Quantity(200)
-    assert Quantity(200) != Quantity(300)
+    assert grams(200) == grams(200)
+    assert grams(200) != grams(300)
+
+def test_grams_are_not_ounces():
+    assert grams(1) != ounces(1)
+
+def grams(amount):
+    return Quantity(amount, "g")
+
+def ounces(amount):
+    return Quantity(amount, "oz")
